f39afa825af50235d58b4a726b5ffd8f722e8f38
nikosoo <130840899+nikosoo@users.noreply.github.com>
2024-07-21 18:14:39 +0300

    asd

--- frontend/src/pages/ProfilePage/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage/ProfilePage.jsx
@@ -10,28 +10,28 @@ const ProfilePage = () => {
   const { userId } = useParams();
 
   return (
-    <div className="bg-gradient-to-r from-purple-400 via-purple-500 to-purple-600 min-h-screen">
+    <div className="bg-gradient-to-r from-purple-300 via-purple-400 to-purple-500 min-h-screen">
       <Navbar />
       <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8 animate__animated animate__fadeIn">
           {/* User Profile and Friend List */}
           <div className="md:col-span-1 space-y-8">
             {/* User Profile */}
-            <div className="bg-purple-50 shadow-md rounded-lg overflow-hidden">
-              <div className="px-6 py-4 bg-purple-700 text-white">
+            <div className="bg-purple-200 shadow-md rounded-lg overflow-hidden">
+              <div className="px-6 py-4 bg-purple-600 text-white">
                 <h3 className="text-lg font-semibold">User Profile</h3>
               </div>
-              <div className="border-t border-purple-200 px-6 py-4">
+              <div className="border-t border-purple-300 px-6 py-4">
                 <UserWidget userId={userId} />
               </div>
             </div>
 
             {/* Friend List */}
-            <div className="bg-purple-50 shadow-md rounded-lg overflow-hidden">
-              <div className="px-6 py-4 bg-purple-700 text-white">
+            <div className="bg-purple-200 shadow-md rounded-lg overflow-hidden">
+              <div className="px-6 py-4 bg-purple-600 text-white">
                 <h3 className="text-lg font-semibold">Friends</h3>
               </div>
-              <div className="border-t border-purple-200 px-6 py-4">
+              <div className="border-t border-purple-300 px-6 py-4">
                 <FriendListWidget userId={userId} isProfilePage={true} />
               </div>
             </div>
@@ -40,11 +40,11 @@ const ProfilePage = () => {
           {/* Posts */}
           <div className="md:col-span-2 space-y-8">
             {/* Posts */}
-            <div className="bg-purple-50 shadow-md rounded-lg overflow-hidden">
-              <div className="px-6 py-4 bg-purple-700 text-white">
+            <div className="bg-purple-200 shadow-md rounded-lg overflow-hidden">
+              <div className="px-6 py-4 bg-purple-600 text-white">
                 <h3 className="text-lg font-semibold">Posts</h3>
               </div>
-              <div className="border-t border-purple-200 px-6 py-4">
+              <div className="border-t border-purple-300 px-6 py-4">
                 <PostsWidget userId={userId} isProfile />
               </div>
             </div>
